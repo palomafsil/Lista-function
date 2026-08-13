625cb2b3ba33b2801e012c9477c904f3c1b73c0e
palomafsil <paloma.f.silva@edu.senai>
2026-08-13 11:13:32 -0300

    Exercicos.js

---
+++ exercicio15.js
@@ -0,0 +1,12 @@
+function classificarNota(nota) {
+    if (nota >= 9) {
+        console.log("Exelente")
+    } else if (nota >= 7) {
+        console.log("Bom")
+        } else if (nota == 6) {
+                console.log("Aprovado")
+            } else{
+                console.log("Reprovado")
+            }
+        }
+classificarNota(8);
