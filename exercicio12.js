83d692da658b4a59872e1f15c51f27021b41c3e5
palomafsil <paloma.f.silva@edu.senai>
2026-08-13 10:51:34 -0300

    Exercicios.js

---
+++ exercicio12.js
@@ -0,0 +1,10 @@
+function verificarNumero(numero) {
+    if (numero > 0) {
+        console.log(" Positivo ");
+    } else if (numero > 0) {
+       console.log(" Negativo ");
+    } else {
+        console.log(" Zero ")
+    }
+}
+verificarNumero(2)
